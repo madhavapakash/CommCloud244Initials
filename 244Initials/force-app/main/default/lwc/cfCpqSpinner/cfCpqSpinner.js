9a765f2c9e155b9dc200b5ff7dfe156de93a7251
Ishita Khare <ikhare@vlocity.com>
2023-10-25 22:37:40 +0530

    Sales_integration_v4_25_10_2023

---
+++ 244Initials/force-app/main/default/lwc/cfCpqSpinner/cfCpqSpinner.js
@@ -0,0 +1,56 @@
+import { FlexCardMixin } from "vlocity_cmt/flexCardMixin";
+    import {interpolateWithRegex, interpolateKeyValue, loadCssFromStaticResource } from "vlocity_cmt/flexCardUtility";
+    
+          import { LightningElement, api, track, wire } from "lwc";
+          import pubsub from "vlocity_cmt/pubsub";
+          import { getRecord } from "lightning/uiRecordApi";
+          
+          import data from "./definition";
+          
+          import styleDef from "./styleDefinition";
+              
+          export default class cfCpqSpinner extends FlexCardMixin(LightningElement){
+              @api debug;
+              @api recordId;
+              @api objectApiName;
+              
+              @track record;
+              
+              
+              pubsubEvent = [];
+              customEvent = [];
+              
+              connectedCallback() {
+                super.connectedCallback();
+                this.setStyleDefinition(styleDef);
+                data.Session = {} //reinitialize on reload
+                
+                
+                
+                this.setDefinition(data);
+ this.registerEvents();
+                
+                
+              }
+              
+              disconnectedCallback(){
+                super.disconnectedCallback();
+                    
+                    
+
+                  this.unregisterEvents();
+              }
+
+              registerEvents() {
+                
+              }
+
+              unregisterEvents(){
+                
+              }
+            
+              renderedCallback() {
+                super.renderedCallback();
+                
+              }
+          }
